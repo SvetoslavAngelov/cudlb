60e740e04358ec5603fc5da94d1df7ab2dcaeb7d
Inex\Svetli <svetoslavangelov86@gmail.com>
2018-09-23 12:34:34 +0100

    Added sort(), partition(), iter_swap()

--- device_algorithm.h
+++ device_algorithm.h
@@ -104,7 +104,7 @@ namespace cudlb
 
 	/**
 	*	Looks for an element with a given value in a range [first, last).
-	*	@[first : last) - range of elements to look for the element
+	*	@[first : last) - range of elements to look for the element.
 	*	@val - element value to look for.
 	*	Returns an iterator to the element if found, otherwise returns last element in the range. 
 	*/
@@ -115,5 +115,58 @@ namespace cudlb
 		for (; first != last; ++first)
 			if (*first == val) return first; 
 		return last; 
+	}
+
+	/**
+	*	Specialization of the cudlb::swap which swaps the values pointed to by the iterators.
+	*	@first - first iterator to swap.
+	*	@second - second iterator to swap.
+	*/
+	template<typename Iterator> 
+	__host__ __device__ 
+	void iter_swap(Iterator first, Iterator second)
+	{
+		using cudlb::swap; 
+		swap(*first, *second);
+	}
+
+	/**
+	*	Tony Hoare's partitioning algorithm, used by cudlb::sort.
+	*	@first - first element of range to partition. 
+	*	@second - last element of range to partition. 
+	*/
+	template<typename Iterator> 
+	__host__ __device__ 
+	Iterator partition(Iterator first, Iterator last)
+	{
+		auto pivot = *first; 
+		auto i = first + 1; 
+		auto j = last - 1; 
+
+		while(i <= j)
+		{
+			while (i <= j && *i <= pivot) ++i;
+			while (i <= j && *i > pivot) --j; 
+			if (i < j) cudlb::iter_swap(i, j);
+		}
+		cudlb::iter_swap(i - 1, first);
+		return i - 1; 
+	}
+
+	/**
+	*	Partitions the range into smaller ones and sorts the values. 
+	*	@first - first element of range to sort.
+	*	@second - last element of range to sort.
+	*/
+	template<typename Iterator> 
+	__host__ __device__
+	void sort(Iterator first, Iterator second)
+	{
+		if (first < last)
+		{
+			auto p = cudlb::partition(first, last);
+			sort(first, p);
+			sort(p + 1, last);
+		}
 	}
 }
